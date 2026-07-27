1f967806c6db7c2a9e494162cfdb794c6fd698fb
Madhu <madhuanbarasu19@gmail.com>
2026-07-28 00:20:37 +0530

    Decreasing triangle

---
+++ PatternPrinting/Pattern09.java
@@ -0,0 +1,17 @@
+package PatternPrinting;
+
+import java.util.Scanner;
+
+public class Pattern09 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=n-i-1;j++){
+                System.out.print("*" +" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
