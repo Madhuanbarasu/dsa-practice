523d11d590c22811d776dbb1d4058e114fedf2fa
Madhu <madhuanbarasu19@gmail.com>
2026-08-03 22:59:15 +0530

    Implemented two sum

--- Arrays/Twosum.java
+++ Arrays/Twosum.java
@@ -1,4 +1,37 @@
 package Arrays;
-
+import java.util.Scanner;
 public class Twosum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int k=0;k<n;k++){
+            arr[k]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        boolean found=false;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==target){
+                   System.out.print(i +" "+ j);
+                   found=true;
+                   break;
+
+                }
+
+
+
+            }
+            if(found) break;
+
+
+
+        }
+        if(!found){
+            System.out.print(" ");
+        }
+
+
+    }
+
 }
